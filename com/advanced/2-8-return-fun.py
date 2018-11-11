bbbf11cc9281aa0c962b897ba2cd4ab6a1007bd4
madison <zhuangsen@live.com>
2018-11-11 18:01:23 +0800

    update

--- com/advanced/2-8-return-fun.py
+++ com/advanced/2-8-return-fun.py
@@ -65,15 +65,16 @@
 # 请编写一个函数calc_prod(lst)，它接收一个list，返回一个函数，返回函数可以计算参数的乘积。
 
 from functools import reduce
+
+
 def calc_prod(lst):
-    def multiSum():
+    def multi_sum():
         def f(x, y):
             return x * y
 
         return reduce(f, lst, 1)
+    return multi_sum
 
-    return multiSum
 
-
-f = calc_prod([1, 2, 3, 4])
-print(f())
+fc = calc_prod([1, 2, 3, 4])
+print(fc())
